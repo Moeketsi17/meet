9f7e8d2f3c5ca41069cfc2e897ff7df91b98781b
moeketsi <raymondkxng17@gmail.com>
2022-11-28 13:55:46 +0200

    edit readme and meetings

--- src/components/Meetings.js
+++ src/components/Meetings.js
@@ -1,21 +1,21 @@
 const Meetings = [
     {
         id: "1",
-        Title: "Project discussion",
-        Description: "Discuss project",
-        Time: "09:30"
+        Title: "Blackdot Studios",
+        Description: "Project discussion",
+        Time: "13:30"
     },
     {
         id: "2",
-        Title: "Budget",
-        Description: "Discuss where budget cuts can be made",
-        Time: "14:30"
+        Title: "Scrum",
+        Description: "Daily scrum meeting",
+        Time: "09:30"
     },
     {
         id: "3",
         Title: "Sales",
-        Description: "Find a way to increase sales",
-        Time: "12:00"
+        Description: "Discuss ways to increase sales",
+        Time: "15:00"
     }
 ]
 
